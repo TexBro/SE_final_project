4b060392272a9c29c2b466c649dc81e49f6b9826
Donghyun Kim <donghyun905@naver.com>
2018-06-03 16:05:08 +0900

    packet json 파일 추가

--- routes/cart.js
+++ routes/cart.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('cart', { title: 'Express' });
 });
 
+router.post('/checkout',function(req,res){
+  var newID=req.body.ID;
+  res.redirect('/checkout')
+});
+
 module.exports = router;
