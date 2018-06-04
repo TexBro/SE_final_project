58369bbb405c0f4e272150f20ec9e33dfde37892
Donghyun Kim <donghyun905@naver.com>
2018-06-04 20:52:43 +0900

    물품 판매 , 리뷰 작성

--- routes/cart.js
+++ routes/cart.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/:id', function(req, res, next) {
+  var id=req.params.id;
+  console.log(id);
   res.render('cart', { title: 'Express' });
 });
 
